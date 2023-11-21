de6a028191796a3aa1c011d2c6b92aab31700829
insurpartsKYS <ys.kim@insurparts.com>
2023-11-21 17:05:12 +0900

    [chore] react query devtools 설치 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Agent from './components/agent';
 
 import './App.css';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Agent />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
